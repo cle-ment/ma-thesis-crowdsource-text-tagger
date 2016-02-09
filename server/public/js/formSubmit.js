78cf73add91f3f20d68790de465abdb59b7fac4c
cle-ment <cle-ment@gmx.de>
2016-02-09 16:30:56 +0200

    update 'thank you' message

--- server/public/js/formSubmit.js
+++ server/public/js/formSubmit.js
@@ -19,20 +19,25 @@ $(function() {
 
         $('#message').hide();
         if (taggedAdsCounter == 1) {
+          $('#hintsRow').hide();
+          $('#exampleRow').hide();
           $('#message').html("<p><strong>Thanks!!! &hearts;</strong>"
-            + " Your tags were saved. </p><p>Want to tag another job ad?"
-            + " That would be great! There's a new one ready "
-            + "for you below.</p>");
+            + " Your tags were saved. </p><p>I'm trying to collect as many tags"
+            + " as possible to better understand how people interpret these"
+            + " texts. So <strong>would be awesome if you could do another"
+            + " one ;)</strong>. Below there's a new job ad already prepared"
+            + " in case</p>");
           $('#message').show();
         } else if (taggedAdsCounter <= 5) {
-          $('#message').html("<p><strong>Thanks again! &hearts;</strong>"
-            + " Your tags were saved. </p><p>Another one? It's ready "
+          $('#message').html("<p><strong>Thanks again for the help! "
+            + "&hearts;</strong></p>"
+            + "<p>Ready for another one? There's a new ad "
             + "for you below.</p>");
           $('#message').show();
         } else if (taggedAdsCounter > 5) {
-          $('#message').html("<p><strong>Wow you're awesome!!!</strong>"
-            + " Everything is saved. </p><p>Still Another one? "
-            + "As always ready for you below.</p>");
+          $('#message').html("<p><strong>Wow so many job ads tagged already!!!"
+            + "</strong>Thanks so much, I really appreciate your help!</p>"
+            + "<p>As always there's a new job ad below.</p>");
           $('#message').show();
         }
         window.scrollTo(0, 0);
