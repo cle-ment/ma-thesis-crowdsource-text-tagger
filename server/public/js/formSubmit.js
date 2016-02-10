c3745cf635066ee296876f5d781fcd5621c87b02
cle-ment <cle-ment@gmx.de>
2016-02-10 12:45:16 +0200

    add error message for empty tags

--- server/public/js/formSubmit.js
+++ server/public/js/formSubmit.js
@@ -12,39 +12,50 @@ $(function() {
       type: "POST",
       url: "http://" + window.location.hostname + ":8082/api/tags",
       data: datastring,
-      success: function(data) {
-        taggedAdsCounter += 1;
-        console.log("Data sent.");
-        getRandomJobAd();
 
-        $('#message').hide();
-        if (taggedAdsCounter == 1) {
-          $.Mustache.load('./templates/thanks1.html')
-          .done(function () {
-              var data = {time: Date.now()}
-              $('#message').mustache('thanks1', data, { method: 'html' });
-              $('#hintsRow').hide();
-              $('#exampleRow').hide();
-              $('#message').show();
-          });
-        } else if (taggedAdsCounter <= 5) {
-          $.Mustache.load('./templates/thanks2.html')
-          .done(function () {
-            var data = {time: Date.now()}
-            $('#message').mustache('thanks2', data, { method: 'html' });
-            $('#message').show();
-          });
-        } else if (taggedAdsCounter > 5) {
-          $.Mustache.load('./templates/thanks3.html')
-          .done(function () {
-            var data = {time: Date.now()}
-            $('#message').mustache('thanks3', data, { method: 'html' });
-            $('#message').show();
+    }).done(function(data) {
+      taggedAdsCounter += 1;
+      console.log("Data sent.");
+      getRandomJobAd();
 
-          });
-        }
-        window.scrollTo(0, 0);
+      $('#message').hide();
+      if (taggedAdsCounter == 1) {
+        $.Mustache.load('./templates/thanks1.html')
+        .done(function () {
+          var data = {time: Date.now()}
+          $('#message').mustache('thanks1', data, { method: 'html' });
+          $('#hintsRow').hide();
+          $('#exampleRow').hide();
+          $('#error').hide();
+          $('#message').show();
+        });
+      } else if (taggedAdsCounter <= 5) {
+        $.Mustache.load('./templates/thanks2.html')
+        .done(function () {
+          var data = {time: Date.now()}
+          $('#message').mustache('thanks2', data, { method: 'html' });
+          $('#error').hide();
+          $('#message').show();
+        });
+      } else if (taggedAdsCounter > 5) {
+        $.Mustache.load('./templates/thanks3.html')
+        .done(function () {
+          var data = {time: Date.now()}
+          $('#message').mustache('thanks3', data, { method: 'html' });
+          $('#error').hide();
+          $('#message').show();
+        });
       }
+      window.scrollTo(0, 0);
+    }).fail(function(response) {
+      $.Mustache.load('./templates/empty.html')
+      .done(function () {
+        var data = {time: Date.now()}
+        $('#error').mustache('empty', data, { method: 'html' });
+        $('#message').hide();
+        $('#error').show();
+        window.scrollTo(0, 0);
+      })
     });
   });
 });
